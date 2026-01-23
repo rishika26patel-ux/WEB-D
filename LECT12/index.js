9028c007ffdf52374d6655e0133c52680b451fdb
rishika26patel <rishika26patel@gmail.com>
2026-01-23 11:09:32 +0530

    js

--- LECT12/index.js
+++ LECT12/index.js
@@ -4,5 +4,5 @@ let btn= document.querySelector('button')
 let body=document.querySelector('body')
 
 btn.addEventListener("click",function(){
-    body.style.backgroundColor='aqua'
+    body.style.backgroundColor='green'
 })
