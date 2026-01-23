05a439809272991bfe652fcff5680b6a13230945
rishika26patel <rishika26patel@gmail.com>
2026-01-23 11:41:48 +0530

    js

--- LECT12/index.js
+++ LECT12/index.js
@@ -2,7 +2,15 @@ console.log("hellow guys!!")
 
 let btn= document.querySelector('button')
 let body=document.querySelector('body')
+let isDark=true
 
 btn.addEventListener("click",function(){
-    body.style.backgroundColor='green'
+    // body.style.backgroundColor='green'
+
+    if(isDark){
+        body.style.backgroundColor='gold'
+    }else{
+        body.style.backgroundColor='maroon'
+    }
+    isDark=!isDark
 })
